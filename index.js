055f0c49545bdd2dd0eead24757c3423b38b2077
JackMarx <seriousfools@gmail.com>
2019-10-04 12:23:33 -0500

    complete

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var moment = require('moment');
 
 console.log("Hello from Javascript");
 
 var datetime = moment().startOf('hour').fromNow();
 console.log(datetime);
+
+import { sqrt } from 'mathjs'
+
+console.log(sqrt(-4).toString()) // 2i
+
+console.log("it did it by it's self...");
